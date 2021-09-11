0944f453435dbdf9d6543906af3bb1518f1e93c8
Petr Pridanov <p.pridanoff2016@yandex.kz>
2021-09-12 02:30:28 +0600

    Create ModalOverlay template

---
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import modalStyles from './modal-overlay.module.css';
+
+function ModalOverlay() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default  ModalOverlay;
