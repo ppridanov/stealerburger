111f52fd0938f225779191119bb5dd6d6b27503d
Petr Pridanov <p.pridanoff2016@yandex.kz>
2021-09-12 02:31:39 +0600

    Create OrderDetails template

---
+++ src/components/order-details/order-details.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import modalStyles from './orders-detail.module.css';
+
+function OrderDetails() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default  OrderDetails;
