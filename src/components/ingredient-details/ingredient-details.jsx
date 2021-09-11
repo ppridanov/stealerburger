97332753bc1a238ee397d43927ccb603b06bc9be
Petr Pridanov <p.pridanoff2016@yandex.kz>
2021-09-12 02:32:34 +0600

    Create IngredientDetails template

---
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import modalStyles from './ingredient-detail.module.css';
+
+function IngredientDetails() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default  IngredientDetails;
