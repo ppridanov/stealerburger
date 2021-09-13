d93c7d4c8dcd495206cb374215bf9c830df07fa2
Petr Pridanov <p.pridanoff2016@yandex.kz>
2021-09-13 23:31:06 +0600

    Now i refactor BurgerIngredients and add Modal with data

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -9,46 +9,61 @@ import Modal from "../modal/modal";
 
 function BurgerIngredients(props) {
     const [current, setCurrent] = React.useState('but');
+    const [modalIsOpen, setModalIsOpen] = React.useState(false);
+    const [modalData, setModalData] = React.useState({});
+    const handleToggleModal = (e) => {
+        const target = e.currentTarget;
+        const id = target.getAttribute('_id');
+        setModalData(props.ingredients.filter((item) => item._id === id));
+        setModalIsOpen(!modalIsOpen);
+    }
     const handleTabClick = (value) => {
         setCurrent(value);
     }
     return (
-        <div className={ingredientsStyles.constr}>
-            <h1 className="text text_type_main-large mt-10">Соберите бургер</h1>
-            <div style={{ display: 'flex' }} className='mt-5'>
-                <a className={appStyles.link} href="#buts">
-                    <Tab value="but" active={current === 'but'} onClick={handleTabClick}>
-                        Булка
-                    </Tab>
-                </a>
-                <a className={appStyles.link} href="#sauces">
-                    <Tab value="sauce" active={current === 'sauce'} onClick={handleTabClick}>
-                        Соусы
-                    </Tab>
-                </a>
-                <a className={appStyles.link} href="#mains">
-                    <Tab value="main" active={current === 'main'} onClick={handleTabClick}>
-                        Начинки
-                    </Tab>
-                </a>
-            </div>
-            <div className={`${ingredientsStyles.ingredients} mt-10`}>
-                <div className={ingredientsStyles.products}>
-                    <h3 className="text text_type_main-medium" id="buts">Булки</h3>
-                    <div className={ingredientsStyles.products__cont}>
-                        {props.ingredients.filter((item) => item.type === 'bun').map((item) => <Ingredient {...item} key={item._id} />)}
-                    </div>
-                    <h3 className="text text_type_main-medium" id="sauces">Соусы</h3>
-                    <div className={ingredientsStyles.products__cont}>
-                        {props.ingredients.filter((item) => item.type === 'sauce').map((item) => <Ingredient {...item} key={item._id} />)}
-                    </div>
-                    <h3 className="text text_type_main-medium" id="mains">Начинки</h3>
-                    <div className={ingredientsStyles.products__cont}>
-                        {props.ingredients.filter((item) => item.type === 'main').map((item) => <Ingredient {...item} key={item._id} />)}
+        <>
+            <div className={ingredientsStyles.constr}>
+                <h1 className="text text_type_main-large mt-10">Соберите бургер</h1>
+                <div style={{ display: 'flex' }} className='mt-5'>
+                    <a className={appStyles.link} href="#buts">
+                        <Tab value="but" active={current === 'but'} onClick={handleTabClick}>
+                            Булка
+                        </Tab>
+                    </a>
+                    <a className={appStyles.link} href="#sauces">
+                        <Tab value="sauce" active={current === 'sauce'} onClick={handleTabClick}>
+                            Соусы
+                        </Tab>
+                    </a>
+                    <a className={appStyles.link} href="#mains">
+                        <Tab value="main" active={current === 'main'} onClick={handleTabClick}>
+                            Начинки
+                        </Tab>
+                    </a>
+                </div>
+                <div className={`${ingredientsStyles.ingredients} mt-10`}>
+                    <div className={ingredientsStyles.products}>
+                        <h3 className="text text_type_main-medium" id="buts">Булки</h3>
+                        <div className={ingredientsStyles.products__cont}>
+                            {props.ingredients.filter((item) => item.type === 'bun').map((item) => <Ingredient onOpen={handleToggleModal} {...item} key={item._id} />)}
+                        </div>
+                        <h3 className="text text_type_main-medium" id="sauces">Соусы</h3>
+                        <div className={ingredientsStyles.products__cont}>
+                            {props.ingredients.filter((item) => item.type === 'sauce').map((item) => <Ingredient onOpen={handleToggleModal} {...item} key={item._id} />)}
+                        </div>
+                        <h3 className="text text_type_main-medium" id="mains">Начинки</h3>
+                        <div className={ingredientsStyles.products__cont}>
+                            {props.ingredients.filter((item) => item.type === 'main').map((item) => <Ingredient onOpen={handleToggleModal} {...item} key={item._id} />)}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            {modalIsOpen && modalData &&
+                <Modal data={modalData} onClose={handleToggleModal}>
+                    1234
+                </Modal>
+            }
+        </>
     );
 
 }
--- src/components/ingredient/ingredient.jsx
+++ src/components/ingredient/ingredient.jsx
@@ -4,9 +4,10 @@ import {CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import PropTypes from 'prop-types';
 
 function Ingredient(props) {
-    const {image, price, name} = props;
+    const {image, price, name, _id} = props;
+
     return (
-        <div className={ingredientStyle.product} >
+        <div className={ingredientStyle.product} onClick={props.onOpen} _id={_id} >
             {/*<Counter />*/}
             <img className={`${ingredientStyle.image} pr-4 pl-4`} src={image} alt=""/>
             <div className={`${ingredientStyle.price} mt-1 mb-1`}>
