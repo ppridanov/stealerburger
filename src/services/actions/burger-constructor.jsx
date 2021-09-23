ebcb70cad0403381de19c04a5366d3acca972a5f
Petr Pridanov <p.pridanoff2016@yandex.kz>
2021-09-24 00:25:51 +0600

    Write burger-constructor actions

---
+++ src/services/actions/burger-constructor.jsx
@@ -0,0 +1,45 @@
+import {sendData} from "../../utils/api";
+import {postOrderURL} from "../../utils/constants";
+
+export const GET_ORDER_REQUEST = 'GET_ORDER_REQUEST';
+export const GET_ORDER_SUCCESS = 'GET_ORDER_SUCCESS';
+export const GET_ORDER_FAILED = 'GET_ORDER_FAILED';
+export const CHANGE_ORDER_INGREDIENTS = 'CHANGE_ORDER_INGREDIENTS';
+
+export const ADD_INGREDIENT_TO_CONSTRUCTOR = 'ADD_INGREDIENT_TO_CONSTRUCTOR';
+export const REMOVE_INGREDIENT_FROM_CONSTRUCTOR = 'REMOVE_INGREDIENT_FROM_CONSTRUCTOR';
+
+export const postOrder = (idsArr) => {
+    return function (dispatch) {
+        dispatch({
+            type: GET_ORDER_REQUEST
+        })
+        sendData({
+            url: postOrderURL,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: {ingredients: idsArr}
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                }
+                dispatch({
+                    type: GET_ORDER_FAILED
+                })
+                throw new Error(`Something wrong: ${res.status}`)
+            })
+            .then(data => dispatch({
+                type: GET_ORDER_SUCCESS,
+                order: data
+            }))
+            .catch(err => {
+                console.log(err);
+                dispatch({
+                    type: GET_ORDER_FAILED
+                })
+            })
+    }
+}
